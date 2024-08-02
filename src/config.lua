3cd4d31329c538d68dc72dc0150a4c7c0e2ba631
Stefan Mielke <stefanmielke@fastmail.com>
2024-08-02 14:36:42 -0300

    movement configurations

--- src/config.lua
+++ src/config.lua
@@ -1,6 +1,16 @@
 local config = {
     debug = {
         show = false
+    },
+    locomotion = {
+        smooth_walk = true,
+        smooth_turn = false,
+        motion_direction = 'head',
+        walk_stick = 'left',
+        turn_stick = 'right'
+    },
+    input = {
+        thumbstick_deadzone = 0.4
     }
 }
 
--- src/locomotion/motion.lua
+++ src/locomotion/motion.lua
@@ -1,13 +1,15 @@
+local config = require 'config'
+
 local motion = {
     pose = lovr.math.newMat4(), -- Transformation in VR initialized to origin (0,0,0) looking down -Z
-    thumbstick_deadzone = 0.4, -- Smaller thumbstick displacements are ignored (too much noise)
-    direction_from = 'head', -- Movement can be relative to orientation of head or left/right controller
 
     -- Snap motion parameters
     snap_turn_angle = 2 * math.pi / 12,
     dash_distance = 1.5,
-    thumbstick_cooldown_time = 0.3,
-    thumbstick_cooldown = 0,
+    thumbstick_cooldown_time_walk = 0.3,
+    thumbstick_cooldown_walk = 0,
+    thumbstick_cooldown_time_turn = 0.3,
+    thumbstick_cooldown_turn = 0,
 
     -- Smooth motion parameters
     turning_speed = 2 * math.pi * 1 / 6,
@@ -18,67 +20,77 @@ function motion.reset(initial_pose)
     motion.pose:set(initial_pose or lovr.math.Mat4())
 end
 
-function motion.smooth(dt)
-    if lovr.headset.isTracked('right') then
-        local x, y = lovr.headset.getAxis('right', 'thumbstick')
-
-        -- Smooth horizontal turning
-        if math.abs(x) > motion.thumbstick_deadzone then
-            motion.pose:rotate(-x * motion.turning_speed * dt, 0, 1, 0)
-        end
-    end
-    if lovr.headset.isTracked('left') then
-        local x, y = lovr.headset.getAxis('left', 'thumbstick')
-        local direction = quat(lovr.headset.getOrientation(motion.direction_from)):direction()
+local function smooth_walk(dt)
+    if lovr.headset.isTracked(config.locomotion.walk_stick) then
+        local x, y = lovr.headset.getAxis(config.locomotion.walk_stick, 'thumbstick')
+        local direction = quat(lovr.headset.getOrientation(config.locomotion.motion_direction)):direction()
         direction.y = 0
 
         -- Smooth strafe movement
-        if math.abs(x) > motion.thumbstick_deadzone then
+        if math.abs(x) > config.input.thumbstick_deadzone then
             local strafeVector = quat(-math.pi / 2, 0, 1, 0):mul(vec3(direction))
             motion.pose:translate(strafeVector * x * motion.walking_speed * dt)
         end
 
         -- Smooth Forward/backward movement
-        if math.abs(y) > motion.thumbstick_deadzone then
+        if math.abs(y) > config.input.thumbstick_deadzone then
             motion.pose:translate(direction * y * motion.walking_speed * dt)
         end
     end
 end
 
-function motion.snap(dt)
-    -- Snap horizontal turning
-    if lovr.headset.isTracked('right') then
-        local x, y = lovr.headset.getAxis('right', 'thumbstick')
-        if math.abs(x) > motion.thumbstick_deadzone and motion.thumbstick_cooldown < 0 then
-            local angle = -x / math.abs(x) * motion.snap_turn_angle
-            motion.pose:rotate(angle, 0, 1, 0)
-            motion.thumbstick_cooldown = motion.thumbstick_cooldown_time
+local function smooth_turn(dt)
+    if lovr.headset.isTracked(config.locomotion.turn_stick) then
+        local x, y = lovr.headset.getAxis(config.locomotion.turn_stick, 'thumbstick')
+
+        -- Smooth horizontal turning
+        if math.abs(x) > config.input.thumbstick_deadzone then
+            motion.pose:rotate(-x * motion.turning_speed * dt, 0, 1, 0)
         end
     end
+end
 
-    -- Dashing forward/backward
-    if lovr.headset.isTracked('left') then
-        local x, y = lovr.headset.getAxis('left', 'thumbstick')
-        if math.abs(y) > motion.thumbstick_deadzone and motion.thumbstick_cooldown < 0 then
-            local move_vector = quat(lovr.headset.getOrientation('head')):direction()
+local function snap_walk(dt)
+    if lovr.headset.isTracked(config.locomotion.walk_stick) then
+        local x, y = lovr.headset.getAxis(config.locomotion.walk_stick, 'thumbstick')
+        if math.abs(y) > config.input.thumbstick_deadzone and motion.thumbstick_cooldown_walk < 0 then
+            local move_vector = quat(lovr.headset.getOrientation(config.locomotion.motion_direction)):direction()
             if not motion.flying then
                 move_vector.y = 0
             end
             move_vector:mul(y / math.abs(y) * motion.dash_distance)
             motion.pose:translate(move_vector)
-            motion.thumbstick_cooldown = motion.thumbstick_cooldown_time
+            motion.thumbstick_cooldown_walk = motion.thumbstick_cooldown_time_walk
         end
     end
 
-    motion.thumbstick_cooldown = motion.thumbstick_cooldown - dt
+    motion.thumbstick_cooldown_walk = motion.thumbstick_cooldown_walk - dt
+end
+
+local function snap_turn(dt)
+    if lovr.headset.isTracked(config.locomotion.turn_stick) then
+        local x, y = lovr.headset.getAxis(config.locomotion.turn_stick, 'thumbstick')
+        if math.abs(x) > config.input.thumbstick_deadzone and motion.thumbstick_cooldown_turn < 0 then
+            local angle = -x / math.abs(x) * motion.snap_turn_angle
+            motion.pose:rotate(angle, 0, 1, 0)
+            motion.thumbstick_cooldown_turn = motion.thumbstick_cooldown_time_turn
+        end
+    end
+
+    motion.thumbstick_cooldown_turn = motion.thumbstick_cooldown_turn - dt
 end
 
 function motion.update(dt)
-    motion.direction_from = lovr.headset.isDown('left', 'trigger') and 'left' or 'head'
-    if lovr.headset.isDown('right', 'grip') then
-        motion.snap(dt)
+    if config.locomotion.smooth_walk then
+        smooth_walk(dt)
     else
-        motion.smooth(dt)
+        snap_walk(dt)
+    end
+
+    if config.locomotion.smooth_turn then
+        smooth_turn(dt)
+    else
+        snap_turn(dt)
     end
 end
 
